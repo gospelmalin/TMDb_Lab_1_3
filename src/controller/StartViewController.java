863bd8912b3db8c26591909f65d4c279165324d8
Malin Albinsson <mea733@telia.com>
2019-01-19 21:55:01 +0100

    Added comments to StartviewController

--- src/controller/StartViewController.java
+++ src/controller/StartViewController.java
@@ -4,37 +4,58 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
+/**
+ * The Class StartViewController handles the start view where the user can choose
+ * what to do.
+ */
 public class StartViewController {
 
+    /** The select search movies button. */
     @FXML
     private Button selectSearchMoviesBtn;
 
+    /** The select search people button. */
     @FXML
     private Button selectSearchPeopleBtn;
     
-  //Initializing the controller class.
-    //This method is automatically called after the fxml file has been loaded.
+
+    /**
+   * Initialize the controller class.
+   */
+  //This method is automatically called after the fxml file has been loaded.
     @FXML
     private void initialize () {
     	
     }
 
+    /**
+     * Select search movies opens the Search movies view.
+     *
+     * @param event the event
+     */
     @FXML
-    void selectSearchMovies(ActionEvent event) {
-    	System.out.println("Search Movie View should open"); //TODO TEMP
+    private void selectSearchMovies(ActionEvent event) {
     	ViewController.activate("SearchMovieView");
     }
 
+    /**
+     * Select search people opens the Search people view.
+     *
+     * @param event the event
+     */
     @FXML
-    void selectSearchPeople(ActionEvent event) {
-    	System.out.println("Search People View should open"); //TODO TEMP
+    private void selectSearchPeople(ActionEvent event) {
     	ViewController.activate("SearchPeopleView");
 
     }
     
+    /**
+     * Select show popular movies opens the Show popular movies view.
+     *
+     * @param event the event
+     */
     @FXML
-    void selectShowPopularMovies(ActionEvent event) {
-    	System.out.println("Show Popular Movies View should open"); //TODO TEMP
+    private void selectShowPopularMovies(ActionEvent event) {
        	ViewController.activate("ShowPopularMovies");
     }
 

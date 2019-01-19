efca1b32d10453b0e5581f0f8b02614c83dc5b5c
Malin Albinsson <mea733@telia.com>
2019-01-19 02:02:17 +0100

    Class Movie

--- src/model/Movie.java
+++ src/model/Movie.java
@@ -34,6 +34,15 @@ public class Movie {
 	/** The vote count. */
 	private int voteCount; // added in preparation for further development
 	
+	/** The budget. */
+	private int budget;
+	
+	/** The status. */
+	private String status;
+	
+	/** The tagline. */
+	private String tagline;
+	
 	/** The vote average. */
 	private double voteAverage; // added in preparation for further development
 	
@@ -150,5 +159,173 @@ public class Movie {
 	 */
 	public void setPopularity(double popularity) {
 		this.popularity = popularity;
+	}
+
+	/**
+	 * @return the adult
+	 */
+	public boolean isAdult() {
+		return adult;
+	}
+
+	/**
+	 * @param adult the adult to set
+	 */
+	public void setAdult(boolean adult) {
+		this.adult = adult;
+	}
+
+	/**
+	 * @return the belongsToCollection
+	 */
+	public Object getBelongsToCollection() {
+		return belongsToCollection;
+	}
+
+	/**
+	 * @param belongsToCollection the belongsToCollection to set
+	 */
+	public void setBelongsToCollection(Object belongsToCollection) {
+		this.belongsToCollection = belongsToCollection;
+	}
+
+	/**
+	 * @return the originalLanguage
+	 */
+	public String getOriginalLanguage() {
+		return originalLanguage;
+	}
+
+	/**
+	 * @param originalLanguage the originalLanguage to set
+	 */
+	public void setOriginalLanguage(String originalLanguage) {
+		this.originalLanguage = originalLanguage;
+	}
+
+	/**
+	 * @return the originalTitle
+	 */
+	public String getOriginalTitle() {
+		return originalTitle;
+	}
+
+	/**
+	 * @param originalTitle the originalTitle to set
+	 */
+	public void setOriginalTitle(String originalTitle) {
+		this.originalTitle = originalTitle;
+	}
+
+	/**
+	 * @return the releaseDate
+	 */
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+
+	/**
+	 * @param releaseDate the releaseDate to set
+	 */
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	/**
+	 * @return the voteCount
+	 */
+	public int getVoteCount() {
+		return voteCount;
+	}
+
+	/**
+	 * @param voteCount the voteCount to set
+	 */
+	public void setVoteCount(int voteCount) {
+		this.voteCount = voteCount;
+	}
+
+	/**
+	 * @return the budget
+	 */
+	public int getBudget() {
+		return budget;
+	}
+
+	/**
+	 * @param budget the budget to set
+	 */
+	public void setBudget(int budget) {
+		this.budget = budget;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public String getStatus() {
+		return status;
+	}
+
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	/**
+	 * @return the tagline
+	 */
+	public String getTagline() {
+		return tagline;
+	}
+
+	/**
+	 * @param tagline the tagline to set
+	 */
+	public void setTagline(String tagline) {
+		this.tagline = tagline;
+	}
+
+	/**
+	 * @return the voteAverage
+	 */
+	public double getVoteAverage() {
+		return voteAverage;
+	}
+
+	/**
+	 * @param voteAverage the voteAverage to set
+	 */
+	public void setVoteAverage(double voteAverage) {
+		this.voteAverage = voteAverage;
+	}
+
+	/**
+	 * @return the genres
+	 */
+	public ArrayList<Object> getGenres() {
+		return genres;
+	}
+
+	/**
+	 * @param genres the genres to set
+	 */
+	public void setGenres(ArrayList<Object> genres) {
+		this.genres = genres;
+	}
+
+	/**
+	 * @return the productionCompanies
+	 */
+	public ArrayList<Object> getProductionCompanies() {
+		return productionCompanies;
+	}
+
+	/**
+	 * @param productionCompanies the productionCompanies to set
+	 */
+	public void setProductionCompanies(ArrayList<Object> productionCompanies) {
+		this.productionCompanies = productionCompanies;
 	}
 }

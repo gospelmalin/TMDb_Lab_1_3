77eb66cafca7afe5ef08e9d2989ba26c2a14d4d5
Malin Albinsson <mea733@telia.com>
2019-01-19 22:01:24 +0100

    Added comments to RootLayoutController

--- src/controller/RootLayoutController.java
+++ src/controller/RootLayoutController.java
@@ -3,19 +3,32 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 
+/**
+ * The Class RootLayoutController.
+ */
 public class RootLayoutController {
 	
-	 //Exit the program
+	 /**
+ 	 * Handle exit.
+ 	 *
+ 	 * @param actionEvent the action event
+ 	 */
+ 	//Exit the program
     public void handleExit(ActionEvent actionEvent) {
         System.exit(0);
     }
  
+    /**
+     * Handle help.
+     *
+     * @param actionEvent the action event
+     */
     //Help Menu button behavior
     public void handleHelp(ActionEvent actionEvent) {
         Alert alert = new Alert (Alert.AlertType.INFORMATION);
         alert.setTitle("Program Information");
         alert.setHeaderText("A JavaFX application to search and view movie information from TMDb made by Malin");
-        alert.setContentText("Credit to TMDb"); //TODO
+        alert.setContentText("Credit to TMDb");
         alert.show();
     }
 }

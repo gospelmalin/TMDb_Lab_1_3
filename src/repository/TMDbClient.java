b6d1839284c0e905f3d818f943623b8aea104aed
Malin Albinsson <mea733@telia.com>
2019-01-19 10:21:09 +0100

    TMDbClient updated with methods for popular movies and people searches

--- src/repository/TMDbClient.java
+++ src/repository/TMDbClient.java
@@ -1,9 +1,13 @@
 package repository;
 
+import java.util.ArrayList;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
+
+import model.Movie;
 
 
 public class TMDbClient {
@@ -15,7 +19,7 @@ public class TMDbClient {
 	
 	/** The start part of the URL used to Search for movies in TMDB. */
 	private static String searchMoviesUrlStart = "https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&query=";
-	
+	private static String popularMoviesUrl = "https://api.themoviedb.org/3/movie/popular?api_key=" + apiKey + "&page=1";
 	private static String searchPeopleUrlStart = "https://api.themoviedb.org/3/search/person?api_key=" + apiKey + "&query=";
 	private static String urlMovieDetailsStart = "https://api.themoviedb.org/3/movie/";
 	private static String urlPersonDetailsStart = "https://api.themoviedb.org/3/person/"; 
@@ -52,11 +56,11 @@ public class TMDbClient {
 	/**
 	 * Query TMDb for movie details.
 	 *
-	 * @param query the query
+	 * @param query the query (id as string)
 	 * @return the string
 	 */
 	public String queryTMDbForMovieDetails(String query) {
-		String URL = urlMovieDetailsStart + query + searchDetailsUrlEnd ;
+		String URL = urlMovieDetailsStart + query + searchDetailsUrlEnd;
 		TMDbClient tc = new TMDbClient();
 		GenericType<String> string = new GenericType<String>() {};
 		String s = tc.client
@@ -67,6 +71,62 @@ public class TMDbClient {
 		//System.out.println("This is the JSON string recieved: " + s); // Kept for reference only
 		return s;
 	}
+
+	/**
+	 * Query TMDb for popular movies.
+	 *
+	 * @param query the query
+	 * @return the string
+	 */
+	public String queryTMDbForPopularMovies() {
+		String URL = popularMoviesUrl;
+		TMDbClient tc = new TMDbClient();
+		GenericType<String> string = new GenericType<String>() {};
+		String s = tc.client
+				.target(URL)
+				.request(MediaType.APPLICATION_JSON)
+				.get(string); // get the JSON representation
+		//print the JSON representation
+		System.out.println("This is the JSON string recieved: " + s); // Kept for reference only
+		return s;
+	}
 	
+	/**
+	 * Query TMDb for people.
+	 *
+	 * @param query the query
+	 * @return the string
+	 */
+	public String queryTMDbForPeople(String query) {
+		String URL = searchPeopleUrlStart + query;
+		TMDbClient tc = new TMDbClient();
+		GenericType<String> string = new GenericType<String>() {};
+		String s = tc.client
+				.target(URL)
+				.request(MediaType.APPLICATION_JSON)
+				.get(string); // get the JSON representation
+		//print the JSON representation
+		System.out.println("This is the JSON string recieved: " + s); // Kept for reference only
+		return s;
+	}
+	
+	/**
+	 * Query TMDb for person details.
+	 *
+	 * @param query the query
+	 * @return the string
+	 */
+	public String queryTMDbForPersonDetails(int id, ArrayList<Movie> knownForMovies) {
+		String URL = urlPersonDetailsStart + id + searchDetailsUrlEnd;
+		TMDbClient tc = new TMDbClient();
+		GenericType<String> string = new GenericType<String>() {};
+		String s = tc.client
+				.target(URL)
+				.request(MediaType.APPLICATION_JSON)
+				.get(string); // get the JSON representation
+		//print the JSON representation
+		//System.out.println("This is the JSON string recieved: " + s); // Kept for reference only
+		return s;
+	}
 }
 
